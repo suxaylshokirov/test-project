e9422e4371cf557364943358bcac9ed83c2d9515
suxaylshokirov <suxaylshokirov@gmail.com>
2024-12-17 21:00:25 +0500

    module 5 nomli papka

---
+++ module_5/test.py
@@ -0,0 +1,11 @@
+print("Welcome to this test python file!")
+
+
+"""
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/suxaylshokirov/test-project.git
+git push -u origin main
+"""
